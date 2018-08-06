1c898335afe068bd2c43f13999dc1fb756d126a3
Trubin Andrey <trubin23@gmail.com>
2018-08-06 23:50:36 +0500

    Initial commit

---
+++ app/src/main/java/ru/trubin23/tasksmvp_kotlin_coroutines/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.trubin23.tasksmvp_kotlin_coroutines
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
